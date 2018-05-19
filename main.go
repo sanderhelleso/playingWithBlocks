df472318ca254720f17c740bfb8deb6825c41620
Sanderhelleso <sanderhelleso@hotmail.com>
2018-05-19 18:11:07 +0200

    added function to calculate block hash

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package playingWithBlocks
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
 // blockChain structure
 type Block struct {
 	Index int
 	Timestamp string
 	Value int
 	Hash string
-	prevHash string
+	PrevHash string
 }
+
+// the main blockchain
+var Blockchain []Block
+
+// calculate the hash of each block
+func calculateHash(block Block) string  {
+	record := string(block.Index) + block.Timestamp + string(block.Value) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
 
