55c1920192d18b26b4ef10917d12ab51b0a34b2a
Sanderhelleso <sanderhelleso@hotmail.com>
2018-05-19 18:36:04 +0200

    got the http server running

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	time2 "time"
+	"os"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"encoding/json"
+	"io"
 )
 
 // blockChain structure
@@ -68,3 +75,62 @@ func replaceChain(newBlock []Block) {
 		Blockchain = newBlock
 	}
 }
+
+// HTTP server
+func run() error {
+	// set router
+	mux := makeMuxRouter()
+	// get port from .env
+	httpAddr := os.Getenv("ADDR")
+	log.Println("Listening on ", os.Getenv(httpAddr))
+
+	// server setup
+	server := &http.Server{
+		Addr: ":" + httpAddr,
+		Handler: mux,
+		ReadTimeout: 10 * time2.Second,
+		WriteTimeout: 10 * time2.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	// check for error
+	if err := server.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// create router
+func makeMuxRouter() http.Handler {
+	muxRouter := mux.NewRouter()
+	// GET request
+	muxRouter.HandleFunc("/", handleGetBlockChain).Methods("GET")
+	// Post request
+	muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
+	return  muxRouter
+}
+
+// GET handler
+func handleGetBlockChain(w http.ResponseWriter, r * http.Request)  {
+	// write back full blockchain in JSON format
+	bytes, err := json.MarshalIndent(Blockchain, "", " ")
+
+	// check for server error
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// write to port
+	io.WriteString(w, string(bytes))
+}
+
+// value function
+type Value struct {
+	Value int
+}
+
+
+
+
