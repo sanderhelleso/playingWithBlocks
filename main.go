30842901dd72951fd79ca374b7d4a829ac28aa29
Sanderhelleso <sanderhelleso@hotmail.com>
2018-05-19 18:15:55 +0200

    set value for new generated block

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package playingWithBlocks
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	time2 "time"
 )
 
 // blockChain structure
@@ -24,6 +25,21 @@ func calculateHash(block Block) string  {
 	h.Write([]byte(record))
 	hashed := h.Sum(nil)
 	return hex.EncodeToString(hashed)
+}
+
+// generates a new block
+func generateBlock(oldBlock Block, Value int) (Block, error) {
+	var newBlock Block
+	time :=  time2.Now()
+
+	// set values for the block
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = time.String()
+	newBlock.Value = Value
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
 }
 
 
